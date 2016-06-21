d85d6330b5f6b40b7f20468a8132278e3d2b0882
JordiCarreraVentura <jordi@immention.com>
2016-06-21 09:09:55 +0200

    Added Workflow module to store general-purpose and procedural functions

---
+++ Workflow.py
@@ -0,0 +1,42 @@
+import json
+
+
+def partition_dataset(dataset, feature_engine, min_ctxt=5, n_macros=300):
+    fe = feature_engine
+    test, train = [], []
+    for i, topic in enumerate(dataset):
+        #print json.dumps(topic, indent=4)
+
+        #	skip if the current topic has fewer than min_ctxt answers
+        #	to use as training to detect that topic:
+        if len(topic['context']) < min_ctxt:
+            continue
+
+        #	collect training instance (answer):
+        for instance in topic['context']:
+            a = instance['answer']
+            triple = (
+                ' '.join(fe(a)),
+                topic['topic'],
+                a
+            )
+            train.append(triple)
+
+        #	collect test instance (question):
+        for instance in topic['data']:
+#             q = instance['answer']
+            q = instance['question']
+            triple = (
+                ' '.join(fe(q)),
+                topic['topic'],
+                q
+            )
+            test.append(triple)
+
+        #	terminate after iterating over n_macros templates:
+        print i
+        if i == n_macros:
+            break
+        print
+    
+    return train, test
