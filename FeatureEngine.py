c82439d71b5fdcf8b96a48cd72129c2b0b753ddf
JordiCarreraVentura <jordi@immention.com>
2016-06-21 09:11:31 +0200

    Disabled n-gram features in FeatureEngine; experimentally proven to hurt performance ~5%

--- FeatureEngine.py
+++ FeatureEngine.py
@@ -34,14 +34,15 @@ class FeatureEngine:
                     grams.append(token)
                 else:
                     grams += [''.join(g) for g in ngrams(token, n)]
-        for n in [2, 3, 4]:
-#         for n in [2]:
-            _grams = ngrams(tokens, n)
-            for _g in _grams:
-                if 'PUNCT' in _g:
-                    continue
-                elif n > 2:
-                    grams.append(' '.join([_g[0], '*', _g[-1]]))
-                else:
-                    grams.append(' '.join(_g))
+#         for n in [1, 2, 3, 4]:
+# #         for n in [2]:
+#             _grams = ngrams(tokens, n)
+#             for _g in _grams:
+#                 if 'PUNCT' in _g:
+#                     continue
+#                 elif n > 2:
+#                     grams.append(' '.join([_g[0], '*', _g[-1]]))
+#                     grams.append(' '.join(_g))
+#                 else:
+#                     grams.append(' '.join(_g))
         return grams
