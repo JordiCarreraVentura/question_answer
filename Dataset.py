02ffaa641794ce4ed32883d0489f25296c24ad8f
JordiCarreraVentura <jordi@immention.com>
2016-06-22 11:43:03 +0200

    Refactored Dataset to apply new sample collection logic

--- Dataset.py
+++ Dataset.py
@@ -1,7 +1,15 @@
+import nltk
+
+import random
 
 from collections import (
+    Counter,
     defaultdict as deft
 )
+
+from nltk import sent_tokenize as splitter
+
+from random import choice
 
 
 class Dataset:
@@ -9,49 +17,64 @@ class Dataset:
 
     def __init__(self, path):
         self.source = path
-        self.templated = deft(list)
-        self.context = deft(list)
+        self._train = []
+        self._test = []
 
 
-    def load(self):
+    def __get_records(self):
+        records = []
         with open(self.source, 'rb') as rd:
             for line in rd:
                 fields = line.decode('utf-8').strip().split('\t')
-
                 if not fields[-1].isdigit():
                     continue
-
-                score = int(fields[-1])
-                topic = fields[3]
-                template = {
-                    'question': fields[1],
-                    'answer': fields[-2],
-                }
-
-                if score:
-                    self.templated[topic].append(template)
-                else:
-                    self.context[topic].append(template)
+                records.append(fields)
+        return records
 
 
-    def __gettitem__(self, topic):
-        return self.templated[topic]
+    def __group_records(self, records):
+        answers = deft(list)
+        counts = Counter()
+        for fields in records:
+            score = int(fields[-1])
+            topic = fields[3]
+            question = fields[1]
+            answer = fields[-2]
+
+            if counts[question] == 2:
+                continue
+
+            if score:
+                answers[question].append(answer)
+                counts[question] += 1
+
+        return answers
 
 
-    def __iter__(self):
-        for topic, data in self.templated.items():
-            if len(data) > 1:
-                yield {
-                    'topic': topic,
-                    'data': data,
-                    'context': self.context[topic],
-                    'type': 'template'
-                }
+    def __partition_dataset(self, answers):
+        for q, aa in answers.items():
+            if len(aa) > 1:
+                test_i = random.randint(0, len(aa) - 1)
+                _a = aa[test_i]
+                __a = aa[:test_i] + aa[test_i + 1:]
+                self._train += [(answer, q, True) for answer in __a]
+                self._test.append((_a, q, True))
             else:
-                yield {
-                    'topic': topic,
-                    'data': data,
-                    'context': self.context[topic],
-                    'type': 'negative_example'
-                }
+                self._test.append((aa[0], q, False))
+
+
+    def load(self):
+        records = self.__get_records()
+        answers = self.__group_records(records)
+        self.__partition_dataset(answers)
+
+
+    def train(self):
+        for record in self._train:
+            yield record
+
+
+    def test(self):
+        for record in self._test:
+            yield record
 
