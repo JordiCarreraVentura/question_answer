e3a9cd5190f64fc42c30d50f86c13a06d74baa64
JordiCarreraVentura <jordi@immention.com>
2016-06-22 11:43:55 +0200

    Modified FeatureEngine to support different parameterizations

--- FeatureEngine.py
+++ FeatureEngine.py
@@ -1,4 +1,6 @@
 
+from collections import defaultdict as deft
+
 from Tools import (
     from_csv,
     ngrams,
@@ -9,11 +11,22 @@ from Tools import (
 )
 
 
+EXCEPTIONS = {
+    'en': ['how', 'what', 'who', 'where', 'when']
+}
+
+
 class FeatureEngine:
-    
-    def __init__(self):
-        return
-    
+
+    def __init__(self, ch_ngrams=True, ngrams=False, lang='en'):
+        self.lang = lang
+        self._ngrams = ngrams
+        self._ch_ngrams = ch_ngrams
+        self.stopwords = deft(bool)
+        for w in STOPWORDS[self.lang]:
+            if w not in EXCEPTIONS[self.lang]:
+                self.stopwords[w] = True
+
     def __call__(self, string):
         space = unicode2ascii(string.lower())
 #         print 1, space
@@ -22,27 +35,47 @@ class FeatureEngine:
         grams = self.__grams(tokens)
 #         print 3, grams
         return grams
-    
+
     def __grams(self, tokens):
         grams = []
-        for token in tokens:
-            for n in [4, 5, 6]:
-#             for n in [4]:
-                if token == 'PUNCT' or token in STOPWORDS['es']:
+        if self._ch_ngrams:
+            for token in tokens:
+                for n in [4, 5, 6]:
+    #             for n in [4]:
+    #                 if token == 'PUNCT' or \
+    #                 self.stopwords[token] or \
+    #                 not token.isalpha():
+    #                     continue
+    #                 if token == 'PUNCT' or \
+    #                 self.stopwords[token]:
+    #                     continue
+                    if token == 'PUNCT':
+                        continue
+                    elif len(token) <= n:
+                        grams.append(token)
+                    else:
+                        grams += [''.join(g) for g in ngrams(token, n)]
+        if not self._ngrams:
+            return grams
+        for n in [2, 3, 4]:
+#         for n in [2]:
+            _grams = ngrams(tokens, n)
+            for _g in _grams:
+                if 'PUNCT' in _g:
                     continue
-                elif len(token) <= n:
-                    grams.append(token)
+                elif n > 2:
+                    grams.append(' '.join([_g[0], '*', _g[-1]]))
                 else:
-                    grams += [''.join(g) for g in ngrams(token, n)]
-#         for n in [1, 2, 3, 4]:
-# #         for n in [2]:
-#             _grams = ngrams(tokens, n)
-#             for _g in _grams:
-#                 if 'PUNCT' in _g:
-#                     continue
-#                 elif n > 2:
-#                     grams.append(' '.join([_g[0], '*', _g[-1]]))
-#                     grams.append(' '.join(_g))
-#                 else:
-#                     grams.append(' '.join(_g))
+                    grams.append(' '.join(_g))
+#         grams = self.__sequence_deduplication(grams)
         return grams
+
+    def __sequence_deduplication(self, grams):
+        new = []
+        prev = None
+        for gram in grams:
+            if gram == prev:
+                continue
+            new.append(gram)
+            prev = gram
+        return new
