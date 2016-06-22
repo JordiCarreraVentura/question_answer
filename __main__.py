3e360b6ea63c78b96e07e1c64b6271255f101fa0
JordiCarreraVentura <jordi@immention.com>
2016-06-22 13:33:25 +0200

    Refactored main method to work with encapsulated task runners

--- __main__.py
+++ __main__.py
@@ -5,12 +5,16 @@ from FirstTask import (
     runner as first_task
 )
 
+from SecondTask import (
+    runner as second_task
+)
+
 
 #KEYS = ('Question', 'Topic', 'Correct', 'Predicted', 'Gold')
 RATIO_TEST_DATA = 0.4
-RATIO_SPECIFICTY = 1.0
+RATIO_SPECIFICITY = 1.0
 RATIO_CONFIDENCE = 0.9
-EXPERIMENTS = 5
+EXPERIMENTS = 4
 PATH_DATA = '/Users/jordi/Laboratorio/corpora/anotados/Microsoft-WikiQA-corpus/WikiQACorpus/WikiQA.tsv'
 
 
@@ -21,7 +25,16 @@ if __name__ == '__main__':
     first_task(
         PATH_DATA,
         RATIO_TEST_DATA,
-        RATIO_SPECIFICTY,
+        RATIO_SPECIFICITY,
+        RATIO_CONFIDENCE,
+        EXPERIMENTS,
+        fe
+    )
+    
+    second_task(
+        PATH_DATA,
+        RATIO_TEST_DATA,
+        RATIO_SPECIFICITY,
         RATIO_CONFIDENCE,
         EXPERIMENTS,
         fe
